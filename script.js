cb5588cc95f4e0c2c5e7fc83e6891a75fd2b6f9f
Jup1t3rTobinC <tobincavanaugh@gmail.com>
2023-07-23 23:42:54 -0700

    aboutme

--- script.js
+++ script.js
@@ -55,3 +55,50 @@ function toggleIcon(imageElement, path1, path2) {
         imageElement.classList.remove("bg-amber-transp");
     }
 }
+
+
+function wrapTextAfterHeading(text) {
+    // Find the position of "###" in the text
+    const headingIndex = text.indexOf("###");
+
+    // If "###" is found, find the position of the next newline character after it
+    if (headingIndex !== -1) {
+        const newlineIndex = text.indexOf("\n", headingIndex);
+
+        // If newline is found, extract the text between "###" and newline
+        if (newlineIndex !== -1) {
+            const textAfterHeading = text.substring(headingIndex + 3, newlineIndex);
+            const wrappedText = `</blockquote><p>${textAfterHeading}</p><blockquote>`;
+            return text.replace(text.substring(headingIndex, newlineIndex + 1), wrappedText);
+        }
+    }
+
+    // If "###" or newline is not found, return the original text
+    return text;
+}
+
+
+function loadMd(textFileURL){
+
+    // Fetch the text file from the URL
+    fetch(textFileURL)
+        .then(response => response.text())
+        .then(text => {
+            // Replace all occurrences of "```" followed by a newline with an empty string
+            text = text.replace(/```\n```/g, '<br/>');
+
+            text = text.replace(/```/g, "");
+
+            //text = text.replace('\n\n', '\n');
+            text = text.replace(/^\s*[\r\n]/gm, '');
+
+            var s = wrapTextAfterHeading(text);
+            // Insert the text into the text-container div
+            document.getElementById('text-container').innerHTML = s;
+        })
+        .catch(error => {
+            // Handle any errors that may occur during the fetch
+            console.error('Error fetching the text file:', error);
+        });
+
+}
