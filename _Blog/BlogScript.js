772845a20caaff16ae38fd6fb138e54a0f1c80a2
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2023-09-10 16:40:58 -0500

    pagesindex

--- _Blog/BlogScript.js
+++ _Blog/BlogScript.js
@@ -1,2 +1,8 @@
-const fs = require('fs');
+const username = "TobinCavanaugh";
+const repoName = "TobinCavanaugh.github.io";
+const subFolder = "_Blog/PagesHTML/";
 
+
+/*
+https://raw.githubusercontent.com/TobinCavanaugh/TobinCavanaugh.github.io/main/_Blog/PagesHTML/Test0.html
+* */
