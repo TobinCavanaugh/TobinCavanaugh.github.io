f4bb5baa9a783903ab022758c1e269adeda572b6
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2023-09-10 18:53:45 -0500

    messing around, i dont think ill take this path

--- _Blog/BlogScript.js
+++ _Blog/BlogScript.js
@@ -1,8 +1,44 @@
+
+import Split from './split.js'
+
 const username = "TobinCavanaugh";
 const repoName = "TobinCavanaugh.github.io";
-const subFolder = "_Blog/PagesHTML/";
+const pagesIndexFile = "_Blog/PagesIndex"
+const htmlFolder = "_Blog/PagesHTML/";
+
+const url = 'https://raw.githubusercontent.com/' + username + '/' + repoName + '/' + 'main' + '/';
+console.log(url);
+
+Split(['#ListView', '#BlogView'])
+
+function BlogSelected(event){
+    console.log("AAAAAAAAAA");
+    console.log(event);
+}
+
+/*Fetch the pages index*/
+fetch(url + pagesIndexFile).then(response => response.text()).then(contents => {
+
+    const splits= contents.split("\n").reverse();
+
+    const list = document.getElementById("DocumentList");
+    splits.forEach(x => {
+        console.log(x);
+        var element = document.createElement("button");
+
+        /* link.href = url + htmlFolder + '/' + x; */
+
+        var data = document.createElement("data");
+        data.value = url + htmlFolder + '/' + x;
+
+        element.textContent = x;
+
+        element.addEventListener("click", BlogSelected)
 
 
-/*
-https://raw.githubusercontent.com/TobinCavanaugh/TobinCavanaugh.github.io/main/_Blog/PagesHTML/Test0.html
-* */
+        element.appendChild(data);
+
+        list.appendChild(element);
+    });
+})
+
