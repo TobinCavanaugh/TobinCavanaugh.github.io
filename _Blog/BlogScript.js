8269d5bd2f6aeeb9f2da982c945d3f9839a786dc
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2023-09-10 19:17:57 -0500

    intel

--- _Blog/BlogScript.js
+++ _Blog/BlogScript.js
@@ -1,13 +1,17 @@
-
 import Split from './split.js'
 
 const username = "TobinCavanaugh";
 const repoName = "TobinCavanaugh.github.io";
 const pagesIndexFile = "_Blog/PagesIndex"
 const htmlFolder = "_Blog/PagesHTML/";
+const randomMessagesFile = "_Blog/RandomMessages";
 
 const url = 'https://raw.githubusercontent.com/' + username + '/' + repoName + '/' + 'main' + '/';
 console.log(url);
 
-var randMessage = document.getElementById("random-message");
-randMessage.R
+var messageElement = document.getElementById("random-message");
+fetch(url + randomMessagesFile).then(x => x.text().then(text => {
+    const messages = text.split('\n');
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    messageElement.textContent = message;
+}))
