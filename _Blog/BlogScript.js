f64ec7d6f8264c09fb11bc18b22ed5422e9b15a5
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2023-09-10 19:08:22 -0500

    added random messages

--- _Blog/BlogScript.js
+++ _Blog/BlogScript.js
@@ -9,36 +9,5 @@ const htmlFolder = "_Blog/PagesHTML/";
 const url = 'https://raw.githubusercontent.com/' + username + '/' + repoName + '/' + 'main' + '/';
 console.log(url);
 
-Split(['#ListView', '#BlogView'])
-
-function BlogSelected(event){
-    console.log("AAAAAAAAAA");
-    console.log(event);
-}
-
-/*Fetch the pages index*/
-fetch(url + pagesIndexFile).then(response => response.text()).then(contents => {
-
-    const splits= contents.split("\n").reverse();
-
-    const list = document.getElementById("DocumentList");
-    splits.forEach(x => {
-        console.log(x);
-        var element = document.createElement("button");
-
-        /* link.href = url + htmlFolder + '/' + x; */
-
-        var data = document.createElement("data");
-        data.value = url + htmlFolder + '/' + x;
-
-        element.textContent = x;
-
-        element.addEventListener("click", BlogSelected)
-
-
-        element.appendChild(data);
-
-        list.appendChild(element);
-    });
-})
-
+var randMessage = document.getElementById("random-message");
+randMessage.R
