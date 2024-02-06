ef5fd00d0b901ef15c2f0a099cbc04ab310c8153
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2024-02-06 13:00:13 -0600

    automation

--- _Blog/BlogScript.js
+++ _Blog/BlogScript.js
@@ -2,16 +2,34 @@
 
 const username = "TobinCavanaugh";
 const repoName = "TobinCavanaugh.github.io";
-const pagesIndexFile = "_Blog/PagesIndex"
 const htmlFolder = "_Blog/PagesHTML/";
 const randomMessagesFile = "_Blog/RandomMessages";
+const pagesIndexFile = "_Blog/PagesIndex";
 
-const url = 'https://raw.githubusercontent.com/' + username + '/' + repoName + '/' + 'main' + '/';
-console.log(url);
+const repoUrl = 'https://raw.githubusercontent.com/' + username + '/' + repoName + '/' + 'main' + '/';
+console.log(repoUrl);
 
 var messageElement = document.getElementById("random-message");
-fetch(url + randomMessagesFile).then(x => x.text().then(text => {
+fetch(repoUrl + randomMessagesFile).then(x => x.text().then(text => {
     const messages = text.split('\n');
     const message = messages[Math.floor(Math.random() * messages.length)];
     messageElement.textContent = message;
+}))
+
+var linksDiv = document.getElementById("Links");
+fetch(repoUrl + pagesIndexFile).then(x => x.text().then(text => {
+    const pages = text.split("\n");
+    for (const p in pages){
+
+        const docName = p.split("|")[0];
+        const fileName = p.split("|")[1];
+
+        const link = document.createElement("a");
+        link.setAttribute("href", repoUrl + htmlFolder + fileName);
+        console.log(link.getAttribute("href"));
+        const node = document.createTextNode(docName);
+        link.appendChild(node);
+
+        linksDiv.appendChild(link);
+    }
 }))
